ef02cd81fe9587cad4b2fe58b3006d490ca42698
Nicholas Marconi <127590111+ThIsIsNoTFiNe@users.noreply.github.com>
2023-12-20 09:56:59 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using marconi.nicholas._4i.LibSlotMachine;
+
+Console.WriteLine("Hello, SlotMachine!");
+
+
+SlotMachine s = new SlotMachine(10);
---
+++ SlotMachine.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace marconi.nicholas._4i.LibSlotMachine
+{
+    public class SlotMachine
+    {
+        //Qua definiamo la SlotMachineù
+        public int credito { get; set; }
+
+        public SlotMachine() 
+        {
+            credito = 0;
+        }
+        public SlotMachine(int credito) 
+        {
+            credito = credito;
+        }
+    }
+}
